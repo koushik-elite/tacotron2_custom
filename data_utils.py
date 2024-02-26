d25b59d706d93bf1759c7f983d0b61fe0253bbb4
koushik <koushik@yahoo.com>
2024-02-26 17:34:06 +0530

    all bug fixes

--- data_utils.py
+++ data_utils.py
@@ -15,6 +15,7 @@ class TextMelLoader(torch.utils.data.Dataset):
         3) computes mel-spectrograms from audio files.
     """
     def __init__(self, audiopaths_and_text, hparams):
+        self.file_path = hparams.dataset_path
         self.audiopaths_and_text = load_filepaths_and_text(audiopaths_and_text)
         self.text_cleaners = hparams.text_cleaners
         self.max_wav_value = hparams.max_wav_value
@@ -31,7 +32,7 @@ class TextMelLoader(torch.utils.data.Dataset):
         # separate filename and text
         audiopath, text = audiopath_and_text[0], audiopath_and_text[1]
         text = self.get_text(text)
-        mel = self.get_mel(audiopath)
+        mel = self.get_mel(f"{self.file_path}/{audiopath}.wav")
         return (text, mel)
 
     def get_mel(self, filename):
--- train.py
+++ train.py
@@ -52,7 +52,7 @@ def prepare_dataloaders(hparams):
         train_sampler = None
         shuffle = True
 
-    train_loader = DataLoader(trainset, num_workers=1, shuffle=shuffle,
+    train_loader = DataLoader(trainset, num_workers=0, shuffle=shuffle,
                               sampler=train_sampler,
                               batch_size=hparams.batch_size, pin_memory=False,
                               drop_last=True, collate_fn=collate_fn)
@@ -124,7 +124,7 @@ def validate(model, criterion, valset, iteration, batch_size, n_gpus,
     model.eval()
     with torch.no_grad():
         val_sampler = DistributedSampler(valset) if distributed_run else None
-        val_loader = DataLoader(valset, sampler=val_sampler, num_workers=1,
+        val_loader = DataLoader(valset, sampler=val_sampler, num_workers=0,
                                 shuffle=False, batch_size=batch_size,
                                 pin_memory=False, collate_fn=collate_fn)
 
@@ -275,7 +275,14 @@ if __name__ == '__main__':
                         required=False, help='comma separated name=value pairs')
 
     args = parser.parse_args()
+
+    # hparams = create_hparams(args.hparams)
+
     hparams = create_hparams(args.hparams)
+
+    hparams.training_files="/mnt/d/Data/Athena_voice/train_audio.txt"
+    hparams.validation_files="/mnt/d/Data/Athena_voice/valid_audio.txt"
+    hparams.dataset_path="/mnt/d/Data/Athena_voice/wavs"
 
     torch.backends.cudnn.enabled = hparams.cudnn_enabled
     torch.backends.cudnn.benchmark = hparams.cudnn_benchmark
